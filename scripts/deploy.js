8c20e976fbc43e9dad6699a93911c362822417bc
AndrejVrkljan <andrej.vrkljan@gmail.com>
2024-09-13 12:47:12 +0200

    deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    const marketplace = await Marketplace.deploy();
+    console.log("Marketplace contract deployed at:", marketplace.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
